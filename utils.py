bf1848af6f1192786ee3a3f375cfe5d20d39f1d4
shannany0606 <ysn24@mails.tsinghua.edu.cn>
2025-02-15 16:30:45 +0800

    full

---
+++ utils.py
@@ -0,0 +1,93 @@
+from tqdm import tqdm
+import torch
+import clip
+
+def cls_acc(output, target, topk=1):
+    pred = output.topk(topk, 1, True, True)[1].t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    acc = float(correct[: topk].reshape(-1).float().sum(0, keepdim=True).cpu().numpy())
+    acc = 100 * acc / target.shape[0]
+    
+    return acc
+
+
+def clip_classifier(classnames, template, clip_model):
+    with torch.no_grad():
+        clip_weights = []
+        for classname in classnames:
+            # Tokenize the prompts
+            classname = classname.replace('_', ' ')
+            texts = [t.format(classname) for t in template]
+            texts = clip.tokenize(texts).cuda()
+            class_embeddings = clip_model.encode_text(texts)
+            class_embeddings /= class_embeddings.norm(dim=-1, keepdim=True)
+            class_embedding = class_embeddings.mean(dim=0)
+            class_embedding /= class_embedding.norm()
+            clip_weights.append(class_embedding)
+        clip_weights = torch.stack(clip_weights, dim=1).cuda()
+        
+    return clip_weights
+
+
+def pre_load_features(clip_model, loader):
+    features, labels = [], []
+    with torch.no_grad():
+        for i, (images, target) in enumerate(tqdm(loader)):
+            images, target = images.cuda(), target.cuda()
+            image_features = clip_model.encode_image(images)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            features.append(image_features.cpu())
+            labels.append(target.cpu())
+        features, labels = torch.cat(features), torch.cat(labels)
+    
+    return features, labels
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author  : AI Partner
+# @Email   : ai.partner.cool@outlook.com
+
+import math
+from copy import deepcopy
+import torch
+import torch.nn as nn
+
+
+class ModelEMA:
+    """
+    Model Exponential Moving Average from https://github.com/rwightman/pytorch-image-models
+    Keep a moving average of everything in the model state_dict (parameters and buffers).
+    This is intended to allow functionality like
+    https://www.tensorflow.org/api_docs/python/tf/train/ExponentialMovingAverage
+    A smoothed version of the weights is necessary for some training schemes to perform well.
+    This class is sensitive where it is initialized in the sequence of model init,
+    GPU assignment and distributed training wrappers.
+    """
+
+    def __init__(self, model, decay=0.9998, updates=0):
+        """
+        Args:
+            model (nn.Module): model to apply EMA.
+            decay (float): ema decay reate.
+            updates (int): counter of EMA updates.
+        """
+        # Create EMA(FP32)
+        self.ema = deepcopy(model).eval()
+        self.updates = updates
+        # decay exponential ramp (to help early epochs)
+        self.decay = lambda x: decay * (1 - math.exp(-x / 2000))
+        for p in self.ema.parameters():
+            p.requires_grad_(False)
+
+    def update(self, model):
+        # Update EMA parameters
+        with torch.no_grad():
+            self.updates += 1
+            decay_value = self.decay(self.updates)
+
+            state_dict = model.state_dict()
+            for k, v in self.ema.state_dict().items():
+                ## in dpp mode, the weight are start with module.
+                if v.dtype.is_floating_point:
+                    v *= decay_value
+                    v += (1.0 - decay_value) * state_dict[k].detach()
